4aef23a9989bcb0c038722833c792aa5a2d8ef36
mabosdavis <78990093+mabosdavis@users.noreply.github.com>
2022-11-17 17:07:48 -0700

    Update VMSJoin.R up to cleaning TIM crash data

--- R/VMSJoin.R
+++ R/VMSJoin.R
@@ -1,5 +1,5 @@
 # load packages
-pacman::p_load(tidyverse, lubridate, zoo)
+pacman::p_load(tidyverse, lubridate, fuzzyjoin)
 
 # load data
 devices <- read_csv("data/vms_devices.csv")
@@ -7,9 +7,57 @@ msg <- read_csv("data/vms_messages.csv")
 msg_2 <- read_csv("data/vms_messages_2.csv")
 
 # clean data
+msg <- msg %>% 
+  select(Device_ID, AIMS_Location, Timestamp, 
+         Category, Condensed, Message)
+
 msg_2 <- msg_2 %>%
-  mutate(AIMS_Location = ...2,
-         Device_ID = `Device ID`,
-         Timestamp = `Date+Time`) %>%
-  filter(Device_ID, AIMS_Location, Timestamp, Length, Category, Condensed, 
-         Message, Location)
+  mutate(Device_ID = ID) %>% 
+  select(Device_ID, AIMS_Location, Timestamp, 
+         Category, Condensed, Message)
+
+# combine data
+messages <- rbind(msg, msg_2)
+
+# make Timestamp a date
+messages <- messages %>% 
+  mutate(Time = mdy_hm(Timestamp, 
+                       tz = 'America/Denver')) %>% 
+  select(Device_ID, AIMS_Location, Time, 
+         Category, Condensed, Message)
+
+# find alt route messages
+messages_tibble <- messages %>% 
+  mutate(Alt = grepl(" alt", messages$Message,
+                     ignore.case = TRUE),
+         Crash = grepl("crash", messages$Message,
+                     ignore.case = TRUE),
+         Caution = grepl("caution", messages$Message,
+                       ignore.case = TRUE))
+
+# create tibble with alt route messages
+messages_alt <- messages_tibble %>%
+  filter(Category == c('Misc','Congestion'),
+         Alt == TRUE)
+
+# load TIM crash data
+crash <- read_csv("data/TIM_clean_2020.csv")
+
+# clean data
+crash <- crash %>% 
+  filter(!is.na(`Total Excess Travel Time`))
+
+# make Date a date
+crash <- crash %>% 
+  mutate(Date = mdy(Date))
+
+crash$datetime <- as.POSIXct(paste(crash$Date, crash$Time),
+                               format = "%Y-%m-%d %H:%M:%S")
+
+# left join crash and message data
+left <- crash %>% 
+  difference_left_join(messages, by = datetime, max_dist = 2, )
+
+
+
+
